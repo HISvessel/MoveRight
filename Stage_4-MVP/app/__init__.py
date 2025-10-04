ae04bd208cc38be37dc9b84f65d9da46d36f83f8
HISvessel <kevinsanchezdabest@yahoo.com>
2025-10-04 16:14:56 -0400

    creating first files and elements for structure, requirements and class tests/API tests.

---
+++ Stage_4-MVP/app/__init__.py
@@ -0,0 +1,4 @@
+"""adding files for structure. This is where the create app function will be placed."""
+
+def create_app():
+    pass
---
+++ Stage_4-MVP/app/models/base_class.py
@@ -0,0 +1,11 @@
+"""This is the base class for all ORM focused entities
+
+Created for the purpose of all tables and databse constraints
+
+Objects it will have:
+1. Creation timestamp
+2. Updating timestamp
+3. object id with UUID"""
+
+class BaseClass():
+    pass
