37ff50591085eca6423cacc0d0f53fac278ddd14
HISvessel <kevinsanchezdabest@yahoo.com>
2025-10-05 15:43:08 -0400

    preparing the baseclass database constraints and schema for sqlite. Added SQLAlchemy instance of object for ORM and database persistent operations with classes.

--- Stage_4-MVP/app/models/base_class.py
+++ Stage_4-MVP/app/models/base_class.py
@@ -1,3 +1,8 @@
+import uuid
+from datetime import datetime
+from app.models.db_model import db
+
+
 """This is the base class for all ORM focused entities
 
 Created for the purpose of all tables and databse constraints
@@ -7,5 +12,21 @@ Objects it will have:
 2. Updating timestamp
 3. object id with UUID"""
 
-class BaseClass():
-    pass
+class BaseClass(db.Model):
+    __abstract__ = True
+    """
+    id = db.Column()
+    created_at = db.Column()
+    updated_at = db.Column()"""
+    def __init__(self):
+        self.id = uuid.uuid4()
+        self.created_at = datetime.date()
+        self.updated_at = datetime.date()
+    
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "created at": self.created_at,
+            "updated at": self.updated_at
+        }
