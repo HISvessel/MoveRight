5cd8347595dfbac7bd051b5ea48d2fc9760f4842
HISvessel <kevinsanchezdabest@yahoo.com>
2025-10-05 17:28:18 -0400

    upgrading baseclass and creating the preliminar structure of the user class

--- Stage_4-MVP/app/models/base_class.py
+++ Stage_4-MVP/app/models/base_class.py
@@ -12,17 +12,29 @@ Objects it will have:
 2. Updating timestamp
 3. object id with UUID"""
 
-class BaseClass(db.Model):
+class BaseClass():#db.Model):
     __abstract__ = True
     """
     id = db.Column()
     created_at = db.Column()
     updated_at = db.Column()"""
     def __init__(self):
+        """temporary constructor method for the base class."""
+
         self.id = uuid.uuid4()
         self.created_at = datetime.date()
         self.updated_at = datetime.date()
     
+    def update(self):
+        """class method for updating the timestamp of updating an element."""
+        self.updated_at = datetime.now()
+
+    def save(self, data):
+        """saving method for overwriting an existing key element in the class object."""
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+            self.save()
 
     def to_dict(self):
         return {
