18d41623b75be972a97624b04061d130c0b4b7de
HISvessel <kevinsanchezdabest@yahoo.com>
2025-10-07 15:40:38 -0400

    added remaining functions for user relational mapping.

--- Stage_4-MVP/app/services/ORM_operations.py
+++ Stage_4-MVP/app/services/ORM_operations.py
@@ -19,5 +19,24 @@ from app.models.review import Review
 up by SQLAlchemy"""
 
 class SQLAlchemyORM(Persistence):
-    def __init_(self):
-        self.user = User()
+    def __init_(self, model):
+        self.model = model
+    
+    def add(self, obj):
+        db.session.add(obj)
+        db.session.commit()
+    
+    def get(self, obj_id):
+        pass
+
+    def get_all(self):
+        pass
+
+    def get_by_attribute(self, attr_name, attr_data):
+        return super().get_by_attribute(attr_name, attr_data)
+    
+    def update(self, attr_name, attr_data):
+        pass
+
+    def delete(self, obj_id):
+        pass
