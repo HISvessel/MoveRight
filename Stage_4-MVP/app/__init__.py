f078ab42f681bdd305c06f134c99878b649286c8
HISvessel <kevinsanchezdabest@yahoo.com>
2025-10-08 00:04:26 -0400

    file template for app was created with the user namespace routes added. Pending database integration into model methods and API calls

--- Stage_4-MVP/app/__init__.py
+++ Stage_4-MVP/app/__init__.py
@@ -1,4 +1,10 @@
+from flask import Flask
+from app.api.user_namespace import user_api
+from flask_restx import add_namespace
 """adding files for structure. This is where the create app function will be placed."""
 
 def create_app():
-    pass
+    app = Flask()
+    app.add_namespace(user_api)
+
+    return app
