0644838c96d003b8da248fcbfb28ea5e04d8a118
HISvessel <kevinsanchezdabest@yahoo.com>
2025-10-08 14:28:34 -0400

    great news, application runs. Now proceeding to test all operations and placing comments on places where mistakes are being present. Standby for further development.

--- Stage_4-MVP/app/__init__.py
+++ Stage_4-MVP/app/__init__.py
@@ -11,7 +11,7 @@ def create_app():
 
     #setting up the app configurations
     app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///move_right.db' 
-    app.config('TESTING') = True
+    app.config['TESTING'] = True
 
     #adding namespaces from the Flask_RESTx api
     api.add_namespace(user_api, path='/users')
