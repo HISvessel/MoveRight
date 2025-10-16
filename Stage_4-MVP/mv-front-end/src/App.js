63dc9fede7583d1c9aa7e5978b8a27ce6f9b7b7c
Joseph Gleason <joeg1834@gmail.com>
2025-10-16 11:40:03 -0400

    added imports state vars, new handler funcs, new routes

--- Stage_4-MVP/mv-front-end/src/App.js
+++ Stage_4-MVP/mv-front-end/src/App.js
@@ -2,18 +2,37 @@ import React, { useState } from 'react';
 import LandingPage from './components/LandingPage';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import Account from './components/Account';
+import Exercise from './components/Exercise';
+import Results from './components/Results';
+import ReviewForm from './components/ReviewForm';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('landing');
   const [user, setUser] = useState(null);
+  const [currentExercise, setCurrentExercise] = useState(null);
+  const [exerciseResults, setExerciseResults] = useState(null);
 
   const handleSignUpSuccess = (userData) => {
-    setUser(userData);
+    const newUser = {
+      ...userData,
+      firstName: userData.name || userData.email.split('@')[0],
+      lastName: '',
+      memberSince: new Date().toISOString()
+    };
+    setUser(newUser);
     setCurrentPage('dashboard');
   };
 
   const handleLoginSuccess = (userData) => {
-    setUser(userData);
+    const newUser = {
+      ...userData,
+      firstName: userData.name || userData.email.split('@')[0],
+      lastName: '',
+      memberSince: new Date().toISOString()
+    };
+    setUser(newUser);
     setCurrentPage('dashboard');
   };
 
@@ -22,22 +41,79 @@ function App() {
     setCurrentPage('landing');
   };
 
+  const handleUpdateUser = (updatedUser) => {
+    setUser(updatedUser);
+  };
+
+  const handleStartExercise = (exercise) => {
+    setCurrentExercise(exercise);
+    setCurrentPage('exercise');
+  };
+
+  const handleExerciseComplete = (results) => {
+    setExerciseResults(results);
+    setCurrentPage('results');
+  };
+  
+
   const renderPage = () => {
     switch(currentPage) {
       case 'landing':
         return <LandingPage onNavigate={setCurrentPage} />;
+      
       case 'signup':
         return <SignUp onNavigate={setCurrentPage} onSignUpSuccess={handleSignUpSuccess} />;
+      
       case 'login':
         return <Login onNavigate={setCurrentPage} onLoginSuccess={handleLoginSuccess} />;
+      
       case 'dashboard':
         return (
-          <div>
-            <h1>Dashboard</h1>
-            <p>Welcome, {user?.name || user?.email}!</p>
-            <button onClick={handleLogout}>Logout</button>
-          </div>
+          <Dashboard 
+            user={user} 
+            onNavigate={setCurrentPage}
+            onStartExercise={handleStartExercise}
+            onLogout={handleLogout}
+          />
         );
+      
+      case 'account':
+        return (
+          <Account 
+            user={user}
+            onNavigate={setCurrentPage}
+            onUpdateUser={handleUpdateUser}
+            onLogout={handleLogout}
+          />
+        );
+      
+      case 'exercise':
+        return (
+          <Exercise 
+            exercise={currentExercise}
+            onNavigate={setCurrentPage}
+            onComplete={handleExerciseComplete}
+          />
+        );
+      
+      case 'results':
+        return (
+          <Results 
+            results={exerciseResults}
+            exercise={currentExercise}
+            onNavigate={setCurrentPage}
+            onStartExercise={handleStartExercise}
+          />
+        );
+      
+      case 'review':
+        return (
+          <ReviewForm 
+            user={user}
+            onNavigate={setCurrentPage}
+          />
+        );
+      
       default:
         return <LandingPage onNavigate={setCurrentPage} />;
     }
